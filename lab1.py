f9f4e69f1b77f8f974baa51fd89174e805e27647
Talkasi <kitalkasi@gmail.com>
2022-09-21 00:44:54 +0300

    Change errors messages

--- lab1.py
+++ lab1.py
@@ -1,23 +1,27 @@
 # Kuracheva Ksenia, ИУ7-13Б
 # V14: Пятиугольная правильная призма, вписанная в цилиндр (даны параметры цилиндра R, h, найти V, Sполн, Sбок).
-import math as m
+import math
 
 print('\tInput area <-> Data\n')
 r_cylinder = float(input("The radius of the cylinder is: "))
+while r_cylinder <= 0:
+    print('\nError occurred. Parameter must be greater than 0.')
+    r_cylinder = float(input("The radius of the cylinder is: "))
+
 h_cylinder = float(input("The height of the cylinder is: "))
+while h_cylinder <= 0:
+    print('\nError occurred. Parameter must be greater than 0.')
+    h_cylinder = float(input("The height of the cylinder is: "))
 
-if r_cylinder <= 0 or h_cylinder <= 0:
-	print('\nError occurred. Parameters must be greater than 0.')
-else:
-	s_prism_base = m.sin(360/5 * m.pi / 180) * r_cylinder * 5 							# Square of the prism base
-	v_prism = h_cylinder * s_prism_base													# Valume of the prism
+s_prism_base = math.sin(360 / 5 * math.pi / 180) * r_cylinder * 5  # Square of the prism base
+v_prism = h_cylinder * s_prism_base  # Valume of the prism
 
-	a_prism = m.sqrt(2 * r_cylinder**2 - 2 * r_cylinder**2 * m.cos(72 * m.pi / 180))	# Side of the prism base
-	s_prism_sdarea = a_prism * h_cylinder * 5											# Square of the prism side area
+a_prism = math.sqrt(2 * r_cylinder ** 2 - 2 * r_cylinder ** 2 * math.cos(72 * math.pi / 180))  # Side of the prism base
+s_prism_side_area = a_prism * h_cylinder * 5  # Square of the prism side area
 
-	s_prism = s_prism_sdarea + s_prism_base												# Square of the prism
+s_prism = s_prism_side_area + s_prism_base  # Square of the prism
 
-	print('\n\tOutput area <-> Result\n')
-	print('The volume of the prism is {:.5g}.'.format(v_prism))
-	print('The square of the prism is {:.5g}.'.format(s_prism))
-	print('The square of the side area of the prism is {:.5g}.'.format(s_prism_sdarea))
+print('\n\tOutput area <-> Result\n')
+print('The volume of the prism is {:.5g}.'.format(v_prism))
+print('The square of the prism is {:.5g}.'.format(s_prism))
+print('The square of the side area of the prism is {:.5g}.'.format(s_prism_side_area))
