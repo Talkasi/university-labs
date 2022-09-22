31c691be24cc3ae6672a687bd86d4e25f47e3399
Talkasi <kitalkasi@gmail.com>
2022-09-22 15:14:39 +0300

    Renaming file to add another version

--- lab2-v_normal.py
+++ lab2-v_normal.py
@@ -13,11 +13,10 @@ if a == 0:  # Special cases
     if b != 0:
         print("\nThe answer is {:.7g}.".format(-c / b))
 else:
-    d = b**2 - 4 * a * c
+    d = b ** 2 - 4 * a * c
     if d < 0:
         print("\nThere is no solution.")
     elif d == 0:
         print("\nThere is one solution: {:.7g}.".format(-b / 2 / a if b != 0 else 0))
     else:
         print("\nThere are two solutions: {:.7g} and {:.7g}.".format((-b + d ** 0.5) / 2 / a, (-b - d ** 0.5) / 2 / a))
-
