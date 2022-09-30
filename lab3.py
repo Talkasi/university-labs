6be0a01de10d17e78bd0c6a8757b38ed2107acab
Talkasi <kitalkasi@gmail.com>
2022-09-30 21:54:52 +0300

    Language corrections

--- lab3.py
+++ lab3.py
@@ -1,7 +1,6 @@
 eps = 1e-9
 
 while True:
-    # Definition
     x1 = float(input("Enter x of the first point: "))
     y1 = float(input("Enter y of the first point: "))
 
@@ -11,7 +10,7 @@ while True:
     x3 = float(input("\nEnter x of the third point: "))
     y3 = float(input("Enter y of the third point: "))
 
-    # Counting sides of the triangle
+    # Compute sides of the triangle
     side1 = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
     side2 = ((x1 - x3) ** 2 + (y1 - y3) ** 2) ** 0.5
     side3 = ((x3 - x2) ** 2 + (y3 - y2) ** 2) ** 0.5
@@ -31,7 +30,7 @@ print("\nThe length of the first side of the triangle is {:.5g}.".format(min_sid
 print("The length of the second side of the triangle is {:.5g}.".format(middle_side))
 print("The length of the third side of the triangle is {:.5g}.".format(max_side))
 
-# Count median size
+# Compute median size
 cos_a = (max_side ** 2 + middle_side ** 2 - min_side ** 2) / 2 / max_side / middle_side
 median = ((max_side / 2) ** 2 + middle_side ** 2 - middle_side * max_side * cos_a) ** 0.5
 print("\nThe length of the median is {:.5g}.".format(median))
@@ -45,12 +44,12 @@ else:
 x = float(input("\nEnter x of the point to check: "))
 y = float(input("Enter y of the point to check: "))
 
-# Count a distance from the point to the triangle vertices
+# Compute a distance from the point to the triangle vertices
 side1_additional = ((x1 - x) ** 2 + (y1 - y) ** 2) ** 0.5
 side2_additional = ((x2 - x) ** 2 + (y2 - y) ** 2) ** 0.5
 side3_additional = ((x3 - x) ** 2 + (y3 - y) ** 2) ** 0.5
 
-# Count the perimeter, area and height of each created triangle
+# Compute the perimeter, area and height of each created triangle
 p_t1 = (side2_additional + side1_additional + side1) / 2
 s_t1 = (p_t1 * (p_t1 - side1) * (p_t1 - side1_additional) * (p_t1 - side2_additional)) ** 0.5
 d1 = 2 * s_t1 / side1
@@ -63,12 +62,12 @@ p_t3 = (side2_additional + side3_additional + side3) / 2
 s_t3 = (p_t3 * (p_t3 - side3) * (p_t3 - side3_additional) * (p_t3 - side2_additional)) ** 0.5
 d3 = 2 * s_t3 / side3
 
-# Count the perimeter, area and height of the given triangle
+# Compute the perimeter, area and height of the given triangle
 p = (side1 + side2 + side3) / 2
 s = (p * (p - side1) * (p - side2) * (p - side3)) ** 0.5
 
 # Check if the point is in the area of the triangle
 if abs(s_t3 + s_t2 + s_t1 - s) <= eps:
-    print("\nThis point is in the area of the triangle. The smallest destination is {:.5g}.".format(min(d1, d2, d3)))
+    print("\nThis point is in the area of the triangle. The smallest distance is {:.5g}.".format(min(d1, d2, d3)))
 else:
     print("\nThis point is out of the area of the triangle.")
