f329040fbe3f705e3adeb6cfad7e7640e8081f49
Talkasi <kitalkasi@gmail.com>
2022-10-06 19:16:18 +0300

    Error solution

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,19 +1,21 @@
+# Kuracheva K.D., ИУ7-13Б, V11
 import math
 
-x_min = 0
-x_max = 1
-x_step = 0.05
+x_min = float(input("Enter x min: "))
+x_max = float(input("Enter x max: "))
+x_step = float(input("Enter x step: "))
 
 # Print a table of values for the functions - x equals to y
-print("┌─────────┬────────────┬────────────┬────────────┐")
-print("│    x    │     y1     │     y2     │     y3     │")
-print("├─────────┼────────────┼────────────┼────────────┤")
+print("┌────────────┬────────────────┬────────────────┬────────────────────┐")
+print("│     x      │       y1       │       y2       │         y3         │")
+print("├────────────┼────────────────┼────────────────┼────────────────────┤")
 
 x = x_min
 y_min = math.inf
 y_max = -math.inf
 
 while x <= x_max + x_step:
+    if x > x_max: break
     y1 = x ** 3 - 6.5 * x ** 2 - 31.3 * x + 2.32
     # Find max and min for scaling y part
     y_min, y_max = min(y_min, y1), max(y_max, y1)
@@ -21,10 +23,10 @@ while x <= x_max + x_step:
     y2 = x ** 2 - math.sin(math.pi * x)
     y3 = y1 ** 2 - y2 ** 2
 
-    print("│_  {:4.2g}  │_ {:6.2g}    │_ {:8.2g}  │_ {:8.2g}  │".format(x, y1, y2, y3))
+    print("│_{:10.3f} │_ {:12.2f}  │_ {:12.2f}  │_ {:16.2f}  │".format(x, y1, y2, y3))
 
     x += x_step
-print("└─────────┴────────────┴────────────┴────────────┘")
+print("└────────────┴────────────────┴────────────────┴────────────────────┘")
 
 # Ask user for the number of '
 while True:
@@ -33,7 +35,7 @@ while True:
     print("\nNumber should be an integer from 4 to 8. Please try again.")
 
 # Start graphic's table
-print("┌─────────┬────────────────────────────────────────────────────────────────────────────────────────┐")
+print("┌────────────┬──────────────────────────────────────────────────────────────────────────────────────────┐")
 
 i = 0
 x = x_min
@@ -56,31 +58,37 @@ while i < n:
     i += 1
 
 # Format s_out
-s_out = "│         │ " + s_out + "│"
+s_out = "│            │ " + s_out + "  │"
 print(s_out)
 
 # Close scaling y part line
-print("├─────────┼────────────────────────────────────────────────────────────────────────────────────────┤")
+print("├────────────┼──────────────────────────────────────────────────────────────────────────────────────────┤")
 
 # Graphic stuff
 scale = 77
 x = x_min
 while x <= x_max + x_step:
+    if x > x_max: break
     y1 = x ** 3 - 6.5 * x ** 2 - 31.3 * x + 2.32
     k = math.ceil(abs(y_min) * scale / (y_max - y_min))
-    k1 = y1 * scale // (y_max - y_min)
+    k1 = math.ceil(y1 * scale / (y_max - y_min))
     k2 = scale - k1
-    if k1 > 0:
-        print("│_  {:4.2g}  │    ".format(x) + " " * int(k) + "|" + " " * int(k1) +
-              "*" + " " * int(scale - k - k1 + 5) + "│")
+    if y_max >= 0:
+        if k1 > 0:
+            print("│_{:10.3f} │    ".format(x) + " " * int(k) + "|" + " " * int(k1) +
+                  "*" + " " * int(scale - k - k1 + 7) + "│")
+        else:
+            p = "" if (int(-k1) == 0) else "*"
+            print("│_{:10.3f} │    ".format(x) + " " * int(k + k1) + p + " " * int(-k1 - 1) +
+                  "|" + " " * (scale - k + 8) + "│")
     else:
-        print("│_  {:4.2g}  │    ".format(x) + " " * int(k + k1) + "*" + " " * int(-k1 - 1) +
-              "|" + " " * 10 + "│")
+        p = "" if (int(scale - k - k1 + 9) == 0) else "*"
+        print("│_{:10.3f} │    ".format(x) + " " * int(k + k1) + p + " " * int(scale - k - k1 + 8) + "│")
+
     x += x_step
 
 # Close graphic's table
-print("└─────────┴────────────────────────────────────────────────────────────────────────────────────────┘")
-
+print("└────────────┴──────────────────────────────────────────────────────────────────────────────────────────┘")
 
 # Found main and max for the additional task
 # Derivative of the first function is  * x ** 2 - 13 * x - 31.3
@@ -91,9 +99,3 @@ x_2 = -1.722778
 y1_1 = x_1 ** 3 - 6.5 * x_1 ** 2 - 31.3 * x_1 + 2.32
 y1_2 = x_2 ** 3 - 6.5 * x_2 ** 2 - 31.3 * x_2 + 2.32
 print("Minimum of the first function is {:.5g}".format(min(y1_1, y1_2)))
-
-
-# Derivative of the second function equals to 0, -> 2 * x - math.pi * math.cos(math.pi * x) = 0
-x = math.cos(math.pi * x) * math.pi / 2
-y2_1 = x ** 2 - math.sin(math.pi * x)
-print("Maximum of the second function is {:.5g}".format(y2_1))
