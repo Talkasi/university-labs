cc656cdb557811a55e5edb903236350ec47eed60
Talkasi <talkasi@h173.net62.bmstu.ru>
2022-10-22 11:17:10 +0300

    lab6 corrections

--- lab6/lab6_4.py
+++ lab6/lab6_4.py
@@ -1,4 +1,6 @@
 # Kuracheva K.D., ИУ7-13Б
+# Find the longest sequense described in V9
+from math import inf 
 
 n = int(input("Enter number of elements in the list: "))
 list_n = [0] * n
@@ -6,25 +8,47 @@ list_n = [0] * n
 for i in range(n):
     list_n[i] = int(input("Enter element #{:g}: ".format(i + 1)))
 
-current_sequence = []
-max_sequence = []
+list_n.append(-inf)
+
+current_len_sequence = 0
+max_len_sequence = 0
+start_i = -1
 for i in range(n):
-    if ((len(current_sequence) > 0 and current_sequence[-1] > list_n[i]) or
-        (len(current_sequence) == 0 and list_n[i] < 0)) and abs(list_n[i]) != 1:
+    if list_n[i] < 0 and abs(list_n[i]) != 1:
+        # Check if the value is prime
         for j in range(2, int(abs(list_n[i]) ** 0.5) + 1):
             if abs(list_n[i]) % j == 0:
-                current_sequence = []
+                current_len_sequence = 0
                 break
         else:
-            current_sequence.append(list_n[i])
+            if (current_len_sequence > 0 and list_n[i - 1] > list_n[i]) or \
+                (current_len_sequence == 0):
+                current_len_sequence += 1
+                if start_i == -1: start_i = i
+                if current_len_sequence > max_len_sequence:
+                    max_len_sequence = current_len_sequence 
+                    searching_start_i = start_i
+
+            elif current_len_sequence > 0 and list_n[i - 1] <= list_n[i]:
+                if current_len_sequence > max_len_sequence:
+                    max_len_sequence = current_len_sequence
+                    searching_start_i = start_i
+
+                current_len_sequence = 1
+                start_i = i
+
+            else:
+                current_len_sequence = 0
+                start_i = -1
     else:
-        current_sequence = []
+        current_len_sequence = 0
+        start_i = -1
 
-    max_sequence = current_sequence if len(current_sequence) > len(max_sequence) else max_sequence
 
-if max_sequence:
+if max_len_sequence:
     print("\nThe longest sequence is:")
-    for i in range(len(max_sequence)):
-        print("Number in the sequence is {:g}, element is {:g}".format(i + 1, max_sequence[i]))
+    for i in range(searching_start_i, searching_start_i + max_len_sequence):
+        print("Number in the sequence is {:g},"
+              " element is {:g}".format(i + 1, list_n[i]))
 else:
     print("\nThere is no such sequence in this list.")
