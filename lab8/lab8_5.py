17d7b91c5e82bda0ac8be799943fd5dc8dcb4778
Talkasi <kitalkasi@gmail.com>
2022-11-01 00:58:42 +0300

    lab8_5

---
+++ lab8/lab8_5.py
@@ -0,0 +1,33 @@
+'''
+5. Найти максимальное значение в квадратной матрице над главной диагональю и
+минимальное - под побочной диагональю.
+'''
+# Kuracheva K.D., ИУ7-13Б
+n = int(input("Enter number of elements in the list: "))
+list_n = [[0.0] * n for i in range(n)]
+
+for i in range(n):
+    print("New line goes: ")
+    for j in range(n):
+        list_n[i][j] = float(input("Enter element}: "))
+
+print("\nEntered matrix is:")
+for i in range(n):
+    s = ""
+    for j in list_n[i]:
+        s += "  {:7.7g}   ".format(j)
+    print(s)
+
+max_el, min_el = 0, 10**20
+for i in range(n):
+    counter = 0
+    for j in range(n):
+        if i < j:
+            max_el = max(max_el, list_n[i][j])
+
+        if n - 1 - i < j:
+            min_el = min(min_el, list_n[i][j])
+
+
+print("Max_el is {:.7g}".format(max_el))
+print("Min_el is {:.7g}".format(min_el))
