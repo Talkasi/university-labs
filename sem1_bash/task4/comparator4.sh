0a4263f00f4ed159c9890fcfe6c839ef023c9306
Talkasi <kitalkasi@gmail.com>
2022-12-20 23:17:43 +0300

    task4

--- sem1_bash/task4/comparator4.sh
+++ sem1_bash/task4/comparator4.sh
@@ -14,23 +14,37 @@
 # $2 stores file2
 # $3 stores verbose key
 
-
+# Checks values given to the skript for errors 
+# and through an error to the stderr and 1 exit value if error occured
 error_check(){
-	local valid_command="Valid command is: comparator2.sh file_name1 file_name2 [-v]"
+	local valid_command="Valid command is: comparator3.sh file_name1 file_name2 [-v]"
 
-	if [ ! \( "$3" == "-v" -o "$3" == "--verbose" -o -z "$3" \) ]; then
-		echo "[!]Error. The key parameter is wrong." >&2
-		echo $valid_command >&2
-
-		exit 1
-	elif [ ! -f "$1" ]; then
-		echo "[!]Error. First parameter is not a file." >&2
-		echo $valid_command >&2
+	if [ ! -f "$1" ]; then
+		echo -e "[!]Error. First parameter is not a file or this file doesn't exist.\n" >&2
+		echo "$valid_command" >&2
 
 		exit 1
 	elif [ ! -f "$2" ]; then
-		echo "[!]Error. Second parameter is not a file." >&2
-		echo $valid_command >&2
+		echo -e "[!]Error. Second parameter is not a file or this file doesn't exist.\n" >&2
+		echo "$valid_command" >&2
+
+		exit 1
+	elif [ ! -r "$1" ]; then
+		echo -e "[!]Error. There is no permission to open the first file.\n" >&2
+
+		exit 1
+	elif [ ! -r "$2" ]; then 
+		echo -e "[!]Error. There is no permission to open the second file.\n" >&2
+
+		exit 1
+	elif [ ! \( "$3" == "-v" -o "$3" == "--verbose" -o -z "$3" \) ]; then
+		echo -e "[!]Error. The key parameter is wrong.\n" >&2
+		echo "$valid_command" >&2
+
+		exit 1
+	elif [ -n "$4" ]; then
+		echo -e "[!]Error. Unexpected parameter after --verbose key.\n" >&2
+		echo "$valid_command" >&2
 
 		exit 1
 	fi
@@ -38,53 +52,53 @@ error_check(){
 
 
 numbers_searcher() {
-	numbers_line=""
-	old_IFS=$IFS
-	IFS=" "
+	file="$(mktemp)"
+	were_numbers=0
 
-	for word in `cat $1`; do
-		number="$(echo "$word" | grep -E '^[+-]?[0-9][0-9]*\.[0-9][0-9]*$')"
-		number+="$(echo "$word" | grep -E '^[+-]?[0-9][0-9]*\.[0-9][0-9]*[eE][+-]?[0-9][0-9]*$')"
-		number+="$(echo "$word" | grep -E '^[+-]?[0-9][0-9]*[eE][+-]?[0-9][0-9]*$')"
+	DONE=false
+	until $DONE; do
+		read -r line || DONE=true
 
-		if [ -n "$number" ]; then
-			numbers_line="$numbers_line$number "
-		fi
-	done
+		for word in $line; do
+			# Check if the word is floating point number or integer or number in scientific notation
+			# Regular expression ^[+-]?[0-9]+(\.[0-9]+)?([eE][+-]?[0-9]+)?$
+			# is used for that
+			if [[ "$word" =~ ^[+-]?[0-9]+(\.[0-9]+)?([eE][+-]?[0-9]+)?$ ]]; then
+	            were_numbers=1
+	            echo "$word" >> "$file"
+	        fi
+		done
+	done < "$1"
 
-	IFS=$old_IFS
+	if [ $were_numbers -eq 0 ]; then 
+		echo "[!]Error. There is no suitable floating point numbers in file '$1'" >&2
 
-	if [ -z "$numbers_line" ]; then 
-		echo "[!]Error. There is no suitable floating point numbers in the file '$1'" >&2
-		
 		exit 1
 	fi
 }
 
 
-error_check "$1" "$2" "$3"
+error_check "$1" "$2" "$3" "$4"
 
-numbers_line=""
+file=""
 
-numbers_searcher $1 $3
-file1_numbers="$numbers_line"
+numbers_searcher $1
+file1="$file"
+numbers_searcher $2
+file2="$file"
 
-numbers_searcher $2 $3
-file2_numbers="$numbers_line"
-
-numbers_in_files_info="Numbers in file1: $file1_numbers\nNumbers in file2: $file2_numbers"
-echo -e $numbers_in_files_info
-
-if [ "$file1_numbers" == "$file2_numbers" ]; then
+if cmp -s "$file1" "$file2" ; then
 	if [ "$3" == "-v" -o "$3" == "--verbose" ]; then
 		echo "[+]Corresponding numbers in given files are equal."
 	fi 
+	rm "$file1" "$file2"
 
 	exit 0
 else
 	if [ "$3" == "-v" -o "$3" == "--verbose" ]; then
 		echo "[-]Corresponding numbers in given files are NOT equal."
 	fi
+	rm "$file1" "$file2"
 
 	exit 1
 fi
---
+++ sem1_bash/task4/test.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+echo "────────────── Welcome to the validation tool of the comparator #4! ──────────────"
+
+
+check() {
+	command="$(echo $?)"
+	[ $command == "$1" ] && echo -e "[PASSED] $2$3" || echo -e \
+	"\n[FAILED] $2$3\n> Expected $1, but $command got.\n"
+}
+
+
+echo -e "─────────── Testing the comparator for errors in the given parameters ────────────\n"
+
+./comparator4.sh > /dev/null 2>&1
+check "1" "No parameters are passed."
+
+./comparator4.sh tests4/file1.txt --verbose > /dev/null 2>&1
+check "1" "One of the files is not specified. Key --verbose is specified."
+
+./comparator4.sh tests4/file1.txt > /dev/null 2>&1
+check "1" "One of the files is not specified. Key --verbose is not specified too."
+
+./comparator4.sh tests4/not_exist.txt tests4/file1.txt > /dev/null 2>&1
+check "1" "The first file does not exist."
+
+./comparator4.sh tests4/file1.txt tests4/not_exist.txt > /dev/null 2>&1
+check "1" "The second file does not exist."
+
+./comparator4.sh tests4/file1.txt tests4/file1.txt -va > /dev/null 2>&1
+check "1" "Key parameter is wrong."
+
+./comparator4.sh tests4/file1.txt tests4/file1.txt --verbose smthing else > /dev/null 2>&1
+check "1" "More parameters are being passed than needed."
+
+echo -e "\n────────────────────────── The main comparator #4 tests ──────────────────────────\n"
+
+# Checked
+./comparator4.sh tests4/empty_file tests4/empty_file > /dev/null 2>&1
+check "1" "Both files are empty."
+
+# Checked
+./comparator4.sh tests4/file1.txt tests4/file1.txt > /dev/null 2>&1
+check "1" "Files are equal and don't contain float numbers."
+
+# Checked
+./comparator4.sh tests4/file2.txt tests4/file3.txt > /dev/null 2>&1
+check "0" "Files are different and don't contain float numbers, but contain equal\n" \
+		  "integers."
+
+# Checked
+./comparator4.sh tests4/file4.txt tests4/file5.txt > /dev/null 2>&1
+check "0" "Files are different and contain equal float numbers and integers."
+
+# Checked
+./comparator4.sh tests4/file6.txt tests4/file7.txt > /dev/null 2>&1
+check "0" "Files are different and contain equal float numbers and integers,\n" \
+		  "numbers in the words should be ignored."
+
+# Checked
+./comparator4.sh tests4/file8.txt tests4/file9.txt > /dev/null 2>&1
+check "0" "Files contain equal float numbers,numbers in scientific notation and\n" \
+		  "integers, numbers in the words should be ignored."
+
+# Checked
+./comparator4.sh tests4/file9.txt tests4/file10.txt > /dev/null 2>&1
+check "1" "Files contain equal float numbers and numbers in scientific notation\n" \
+		  "with different number of zeros in the end."
+
+# Checked
+./comparator4.sh tests4/file9.txt tests4/file13.txt > /dev/null 2>&1
+check "1" "Files contain different float numbers and numbers in scientific notation."
+
+# Checked
+./comparator4.sh tests4/file11.txt tests4/file12.txt > /dev/null 2>&1
+check "0" "Files contain float numbers and numbers in scientific notation with\n" \
+		  "leading zeros."
+
+echo -e "\n───────────────────────────── All tests have been run ────────────────────────────" \
+		"\n──────────────────────────────────── Good luck! ──────────────────────────────────"
