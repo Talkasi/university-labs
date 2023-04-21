7e9f4a4a37a93c57d2996505225b16fd6ed96c9c
Talkasi <kitalkasi@gmail.com>
2023-04-21 20:02:53 +0300

    Python lab03 protection improved

--- sem2_python/lab03_protection/logic.py
+++ sem2_python/lab03_protection/logic.py
@@ -1,36 +1,16 @@
 from PIL import Image
+from PIL import ImageDraw
+
+color = "green"
 
 img = Image.open('cat.png').convert("RGB")
 img_size = img.size
 
-step_x = int(img_size[1] / 7)
-step_y = int(img_size[0] / 7)
-for y in range(0, img_size[1]):
-    for x in range(0, img_size[0]):
-        pix = img.getpixel((x, y))
+draw = ImageDraw.Draw(img)
+for i in range(0, img_size[1], img_size[1] //  7):
+    draw.line((0, i) + (img_size[0], i), width=5, fill=color)
 
-        if step_x - 20 < x < step_x or step_y - 20 < y < step_y:
-            pix = (255, 0, 0)
-            if x == step_x - 1:
-                step_x += int(img_size[1] / 7)
-            if y == step_y - 1:
-                step_y += int(img_size[0] / 7)
-
-        img.putpixel((x, y), pix)
-
-step_x = int(img_size[1] / 7)
-step_y = int(img_size[0] / 7)
-for x in range(0, img_size[0]):
-    for y in range(0, img_size[1]):
-        pix = img.getpixel((x, y))
-
-        if step_x - 20 < x < step_x or step_y - 20 < y < step_y:
-            pix = (255, 0, 0)
-            if x == step_x - 1:
-                step_x += int(img_size[1] / 7)
-            if y == step_y - 1:
-                step_y += int(img_size[0] / 7)
-
-        img.putpixel((x, y), pix)
+for i in range(0, img_size[0], img_size[0] //  7):
+    draw.line((i, 0) + (i, img_size[1]), width=5, fill=color)
 
 img.save('new_cat.png', "PNG")
