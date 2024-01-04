4d9ea67407cd94dd775dcdd1f715ba1c0cbd621b
Talkasi <kitalkasi@gmail.com>
2024-01-04 11:42:51 +0300

    Compare function implemented

--- sem3_types_and_data_structure/lab_01_rewritten/big_nums.c
+++ sem3_types_and_data_structure/lab_01_rewritten/big_nums.c
@@ -1,5 +1,35 @@
 #include "big_nums.h"
-#include <assert.h>
+
+int get_digit(bdouble_t *d, int digit_pos)
+{
+	int i = digit_pos / DIGITS_PER_LONG;
+	int shift = DIGITS_PER_LONG - BITS_PER_DIGIT - (digit_pos % DIGITS_PER_LONG);
+	return (d->mantissa[i] >> shift) & DIGIT_MASK;
+}
+
+void put_digit_to_end(bdouble_t *d, int digit)
+{
+	int i = digit_pos % DIGITS_PER_LONG;
+	int shift = DIGITS_PER_LONG - BITS_PER_DIGIT - (digit_pos % DIGITS_PER_LONG);
+
+	d->mantissa[i] |= (digit & DIGIT_MASK) << shift;
+	++d->d_in_mantissa;
+}
+
+int cmp_bdoubles(bdouble_t *d1, bdouble_t *d2)
+{
+	if (d1->d_in_mantissa != d2->d_in_mantissa)
+		return d1->d_in_mantissa - d2->d_in_mantissa;
+
+	for (int i = 0; i < d1->d_in_mantissa; ++i) {
+		d1_digit = get_digit(d1, i);
+		d2_digit = get_digit(d2, i);
+		if (d1_digit != d2_digit)
+			return d1_digit - d2_digit;
+	}
+
+	return 0;
+}
 
 int div_bdoubles(bdouble_t *res, bdouble_t d1, bdouble_t d2)
 {
@@ -16,19 +46,19 @@ int div_bdoubles(bdouble_t *res, bdouble_t d1, bdouble_t d2)
 	int new_digit;
 	do {
 		while (part_div.d_in_mantissa < d2.d_in_mantissa) {
-			if (next_d1_digit_i >= d1.d_in_mantissa && is_zero(part_div))
+			if (next_d1_digit_i >= d1.d_in_mantissa && (part_div.d_in_maintissa == 0))
 				return 0;
 
 			new_digit = (next_d1_digit_i < d1.d_in_mantissa) ? get_digit(d1, next_d1_digit_i++) : 0;
-			if (!is_zero(part_div) || ((is_zero(part_div) && new_digit != 0)))
-				put_digit(&part_div, part_div.d_in_mantissa, new_digit);
+			if (!(part_div.d_in_maintissa == 0) || (((part_div.d_in_maintissa == 0) && new_digit != 0)))
+				put_digit_to_end(&part_div, new_digit);
 		}
 
 		simple_dividend = get_digit(part_div, 0);
 		if (simple_dividend < simple_divisor) {
 			simple_dividend = (simple_dividend * 10) + get_digit(part_div, 1);
 			new_digit = (next_d1_digit_i < d1.d_in_mantissa) ? get_digit(d1, next_d1_digit_i++) : 0;
-			put_digit(&part_div, part_div.d_in_mantissa, new_digit);
+			put_digit_to_end(&part_div, new_digit);
 		}
 
 		simple_multiplier = simple_dividend / simple_divisor;
@@ -36,10 +66,9 @@ int div_bdoubles(bdouble_t *res, bdouble_t d1, bdouble_t d2)
 		while (cmp_bdoubles(tmp_mul_res, d2) > 0)
 			mul_bdouble(&tmp_mul_res, part_div, --simple_multiplier);
 
-		put_digit(res, res.d_in_mantissa, simple_multiplier);
+		put_digit_to_end(res, simple_multiplier);
 		sub_bdoubles(&part_div, tmp_mul_res);
-	} while (next_d1_digit_i < d1.d_in_mantissa || !is_zero(part_div));
+	} while (next_d1_digit_i < d1.d_in_mantissa || !(part_div.d_in_maintissa == 0));
 
 	return 0;
 }
-
